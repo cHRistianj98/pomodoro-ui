40f1a8ee5c866a400b80538dea34cf32378f97a7
vanvi <kania.praca@wp.pl>
2024-05-07 16:57:08 +0200

    Create taskCreation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TimerComponent} from "./timer/timer.component";
 import {AppModule} from "./app.module";
+import {TaskCreationComponent} from "./task-creation/task-creation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TimerComponent, AppModule],
+  imports: [CommonModule, RouterOutlet, TimerComponent, AppModule, TaskCreationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/task-creation/task-creation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-creation',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-creation.component.html',
+  styleUrl: './task-creation.component.css'
+})
+export class TaskCreationComponent {
+
+}
