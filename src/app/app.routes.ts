14d1f4d3719a25c71908921f1ce59fd63df10549
Kania <kania.praca@wp.pl>
2025-04-28 19:00:33 +0200

    Timer page is available only for logged users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent }        from './login/login.component';
 import { TimerComponent } from "./timer/timer.component";
+import { authGuard }             from './core/auth.guard';
 
 export const routes: Routes = [
   { path: '',          component: LoginComponent },
   { path: 'register',  component: RegistrationComponent },
   { path: 'login',     component: LoginComponent },
-  { path: 'timer',     component: TimerComponent },
+  { path: 'timer',     component: TimerComponent, canActivate: [authGuard] },
   { path: '**',        redirectTo: '' }
 ];
